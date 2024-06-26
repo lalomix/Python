2092c2cfab1ad637980a5807362bc0b509254a92
Eduardo Vargas <38983641+lalomix@users.noreply.github.com>
2024-06-25 23:20:56 -0400

    2

--- clase_1906/asistencia_csv.py
+++ clase_1906/asistencia_csv.py
@@ -4,19 +4,13 @@ from datetime import datetime
 now = datetime.now()
 alumnos=[]
 fecha_asistencia=[]
-
+fecha_seteada=now.strftime('%d-%m-%Y')
 #Funciones
 
-def guardar_asistencia(alumno):
-    alumnos.append(alumno)
-    fecha_asistencia.append(now.strftime('%d-%m-%Y'))
-    with open(f'Asistencia-{fecha_asistencia}.csv', 'w') as file:
+def guardar_asistencia(alumnos):    
+    with open(f'Asistencia-{fecha_seteada}.csv', 'w') as file:
         for i in range(len(alumnos)):
-            file.write(f"{alumnos[i]} - {fecha_asistencia[i]} \n")
-        
-    
-    
-    
+            file.write(f"{alumnos[i]},{fecha_asistencia[i]} \n")
 
 def buscar_asistencia(alumno):
     if alumno in alumnos:
@@ -29,6 +23,7 @@ validador = True
 validador2 = True
 while validador:
     print("\n")
+    print(fecha_seteada)
     print("1. Guardar Asistencia ")
     print("2. Buscar Asistencia ")
     print("3. Salir ")
@@ -42,8 +37,11 @@ while validador:
                 alumno=input("Ingrese nombre y apellido de alumno que desea registar,\n escriba terminar para finalizar: ").lower()
                 if alumno == "terminar":
                     validador2 = False
-                else: 
-                    guardar_asistencia(alumno)
+                    guardar_asistencia(alumnos)
+                else:
+                    alumnos.append(alumno)
+                    fecha_asistencia.append(fecha_seteada)
+                    
         case 2:
             os.system("cls")
             alumno=input("Ingrese el nombre y el apellido del alumno que desea buscar el registro de asistencia : ")
